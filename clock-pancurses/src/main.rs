3cfdd54a83bdcae53746825cc9b233821f893da7
Kristopher Johnson <kris@kristopherjohnson.net>
2019-03-21 20:31:10 -0400

    Use bold yellow text

--- clock-pancurses/src/main.rs
+++ clock-pancurses/src/main.rs
@@ -6,7 +6,10 @@ extern crate pancurses;
 
 use chrono::{Local, Utc};
 
-use pancurses::{curs_set, endwin, initscr};
+use pancurses::{
+    curs_set, endwin, has_colors, init_pair, initscr, start_color, A_BOLD,
+    COLOR_BLACK, COLOR_PAIR, COLOR_YELLOW,
+};
 
 use std::thread;
 use std::time::Duration;
@@ -16,6 +19,13 @@ fn main() {
     window.nodelay(true);
 
     curs_set(0);
+
+    if has_colors() {
+        // Display bold yellow text
+        start_color();
+        init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+        window.attron(COLOR_PAIR(1) | A_BOLD);
+    }
 
     let time_format = "%Y-%m-%d %H:%M:%S%.3f %z";
     loop {
